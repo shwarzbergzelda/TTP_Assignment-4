b2437f09235a2e8abd086c66d00b7a7c8754b3c9
zelda <zeldashw@gmail.com>
2022-01-10 02:24:48 -0500

    included additional clarifying comments into various code snippets/ functions in arguingButtons and sphereVolume HTML and JS files.

--- scripts/arguingButtons.js
+++ scripts/arguingButtons.js
@@ -1,7 +1,3 @@
-/* Create functionality for HTML page with two buttons that argue with each other. 
-When one button is clicked, the text "I'm right" should be placed next to
-it. When the other button is clicked, the text is replaced with, "No, I'm right!" */
-
 window.onload = function() {
     const arguingButton1 = document.getElementById("arguing-button-1");
     const arguingButton2 = document.getElementById("arguing-button-2");
@@ -33,11 +29,15 @@ window.onload = function() {
     function generateArgumentText(currentButtonClicked) {
         if (argumentText.textContent === "") {
             argumentText.textContent = "I'm right";
-        } else if (currentButtonClicked == prevButtonClicked) {
+        } else if (currentButtonClicked == prevButtonClicked) { /* Prevents arguing against oneself */
             alert("Disputant cannot argue against themself!");
         } else {
             argumentText.textContent = "No, I'm right!";
         }
     }
+
+    /* Create functionality for HTML page with two buttons that argue with each other. 
+    When one button is clicked, the text "I'm right" should be placed next to
+    it. When the other button is clicked, the text is replaced with, "No, I'm right!" */
 
 }
--- scripts/sphereVolume.js
+++ scripts/sphereVolume.js
@@ -3,11 +3,11 @@ window.onload = function() {
     const displayVolume = document.getElementById("display-volume");
     const submitRadius = document.getElementById("submit-radius");
     const inputForm = document.getElementById("input-form");
-    
-    /* Calculates and displays sphere volume based on input sphere radius */
 
+    /* calls function to calculate and display item if submit button is clicked */
     submitRadius.addEventListener("click", calculateDisplayVolume)
 
+    /* calls function to calculate and display item if "enter" key is pressed */
     inputForm.addEventListener("keypress", (event) => {
         if (event.code === "Enter") {
             event.preventDefault();
@@ -16,17 +16,19 @@ window.onload = function() {
     });
     
     function calculateDisplayVolume() {
+        /* Ensures valid input */
         if (sphereRadius.value==="") {
             alert("Please enter radius");
         } else if (sphereRadius.value < 0) {
             alert("Radius cannot be less than 0. Please enter a positive number.")
         } else if (isNaN(sphereRadius.value)) {
             alert("Please enter a valid number");
-        } else {
+        } else { /* If input is valid, calculates and displays volume */
             const sphereVolume = ((4/3) * Math.PI * Math.pow(sphereRadius.value, 3));
             displayVolume.textContent = 
                 (`The volume of a sphere with radius ${sphereRadius.value} is: ${sphereVolume.toLocaleString()}`);
             sphereRadius.value="";
         }
     }
+    /* Calculates and displays sphere volume based on input sphere radius */
 }
